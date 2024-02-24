5b8e854c37e8f9c6cdd9b05b036e28e9b05a290c
Kjohnson67 <kdj95550@ucmo.edu>
2024-02-23 19:27:55 -0600

    updaes on author domain

--- src/main/java/com/example/demo/domain/Writer.java
+++ src/main/java/com/example/demo/domain/Writer.java
@@ -1,7 +1,38 @@
 package com.example.demo.domain;
 
-public class Author {
+public class Writer {
     //initially, the file name Author is red.
     //it means that this file has not been added to github
     //then, let's manually add it
+    //after doing so, the file name turns green, because it has been added
+    //next, lets manually commit it
+    //after commiting, the file names turns blue, which means the file has been committed
+
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

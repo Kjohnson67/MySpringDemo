ab1bbb0ae68457ca76ba04f7d95cd8620dc0ad38
Kjohnson67 <kdj95550@ucmo.edu>
2024-02-23 19:12:37 -0600

    Adding new attributes/properties for book domain

--- src/main/java/com/example/demo/domain/Book.java
+++ src/main/java/com/example/demo/domain/Book.java
@@ -13,9 +13,55 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 public class Book {
+    //test git command adds some new properties to the book domain
+    //when adding a new file or updating an existing file under git env,
+    //i strongly suggest you to go to the end path everytime
+    //because it aviods making unnecessary changes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String author;
+    private double price;
+    private String publisher;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 }

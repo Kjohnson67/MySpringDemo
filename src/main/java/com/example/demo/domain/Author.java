cec8952719a60e24022798e8ea55147c85c2d63b
Kjohnson67 <kdj95550@ucmo.edu>
2024-02-23 19:22:16 -0600

    Create Author domain

---
+++ src/main/java/com/example/demo/domain/Author.java
@@ -0,0 +1,7 @@
+package com.example.demo.domain;
+
+public class Author {
+    //initially, the file name Author is red.
+    //it means that this file has not been added to github
+    //then, let's manually add it
+}
